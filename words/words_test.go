7f0a4b75a8760857b4a1cd5d3514bfb9dd0f25e6
Calissa Fairburn <calissafairburn@Calissas-MacBook-Pro.local>
2022-10-20 20:16:08 +0100

    tests collison map, characters in words func, words in anagram func and word list

--- words/words_test.go
+++ words/words_test.go
@@ -85,88 +85,6 @@ func Test_Words_CheckUnique(t *testing.T) {
 	}
 }
 
-func Test_Words_checkCharInWord(t *testing.T) {
-	type args struct {
-		wordList []string
-		newWord  string
-	}
-
-	type testCase struct {
-		args args
-		want bool
-	}
-
-	testCases := map[string]testCase{
-		"returns true is any character matches between new word and word list": {
-			args: args{
-				wordList: []string{
-					"those",
-					"brick",
-					"jumpy",
-				},
-				newWord: "chunk",
-			},
-			want: true,
-		},
-		"returns false if no character matches between new word and word list": {
-			args: args{
-				wordList: []string{
-					"those",
-					"brick",
-					"jumpy",
-				},
-				newWord: "gland",
-			},
-			want: false,
-		},
-	}
-
-	for name, tc := range testCases {
-		t.Run(name, func(t *testing.T) {
-			got := checkCharInWord(tc.args.wordList, tc.args.newWord)
-			assert.Equal(t, tc.want, got)
-		})
-	}
-}
-
-func Test_Words_WordList(t *testing.T) {
-	type args struct {
-		words map[string][]string
-	}
-	type testCase struct {
-		args args
-		want []string
-	}
-
-	testCases := map[string]testCase{
-		"returns list of words with all distinct characters from word list": {
-			args: args{
-				words: map[string][]string{
-					"abcde": {"abcde", "edcba"},
-					"fghij": {"fghij", "jihgf"},
-					// duplicate words
-					"abdde": {"abdde", "eddbe"},
-					"klghj": {"klghj"},
-					//
-					"klmno": {"klmmo", "onmlk"},
-					"pqrst": {"pqrst", "tsrqp"},
-				},
-			},
-			want: []string{
-				"abcde",
-				"pqrst",
-				"klmno",
-			},
-		},
-	}
-	for name, tc := range testCases {
-		t.Run(name, func(t *testing.T) {
-			got := WordList(tc.args.words)
-			assert.Equal(t, tc.want, got)
-		})
-	}
-}
-
 func Test_Words_GenerateAnagrams(t *testing.T) {
 	type args struct {
 		wordList []string
@@ -208,6 +126,160 @@ func Test_Words_GenerateAnagrams(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			got := GenerateAnagrams(tc.args.wordList)
 
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_Words_FindCollisions(t *testing.T) {
+	type args struct {
+		anagrams map[string][]string
+	}
+	type testCase struct {
+		args args
+		want map[string][]string
+	}
+
+	testCases := map[string]testCase{
+		"returns a collision map with anagram keys and values that don't collide with the anagram": {
+			args: args{
+				anagrams: map[string][]string{
+					"abcde": {"abcde", "edcba"},
+					"fghij": {"fghij", "jihgf"},
+					"abdde": {"abdde", "eddbe"},
+				},
+			},
+			want: map[string][]string{
+				"abcde": {"fghij"},
+				"fghij": {"abcde", "abdde"},
+				"abdde": {"fghij"},
+			},
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := FindCollisions(tc.args.anagrams)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_Words_checkCharInWord(t *testing.T) {
+	type args struct {
+		originalWord   string
+		comparisonWord string
+	}
+	type testCase struct {
+		args args
+		want bool
+	}
+
+	testCases := map[string]testCase{
+		"returns true is any character matches between comparison word and original word": {
+			args: args{
+				originalWord:   "abcde",
+				comparisonWord: "hiaok",
+			},
+			want: true,
+		},
+		"returns false if no characters match between comparison word and original word": {
+			args: args{
+				originalWord:   "abcde",
+				comparisonWord: "fghij",
+			},
+			want: false,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := checkCharInWord(tc.args.originalWord, tc.args.comparisonWord)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_Words_checkWordInAnagrams(t *testing.T) {
+	type args struct {
+		anagram        string
+		comparisonWord string
+	}
+	type testCase struct {
+		args args
+		want bool
+	}
+
+	testCases := map[string]testCase{
+		"returns a true if word is in list of anagrams": {
+			args: args{
+				anagram:        "fghij",
+				comparisonWord: "jihgf",
+			},
+			want: true,
+		},
+		"returns a false if word is not in list of anagrams": {
+			args: args{
+				anagram:        "abcde",
+				comparisonWord: "lmnop",
+			},
+			want: false,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := checkWordInAnagram(tc.args.anagram, tc.args.comparisonWord)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_Words_WordList(t *testing.T) {
+	type args struct {
+		collisonMap  map[string][]string
+		fullWordList []string
+	}
+	type testCase struct {
+		args args
+		want []string
+	}
+	testCases := map[string]testCase{
+		"returns list of character distinct words": {
+			args: args{
+				collisonMap: map[string][]string{
+					"lmnop": {"fghij"},
+					"fghij": {"abcde", "abdde"},
+					"abdde": {"fghij"},
+					"abcde": {"efghi", "jklmn", "opqrs", "tuvwx"},
+				},
+				fullWordList: []string{
+					"lmnop",
+					"fghij",
+					"fghij",
+					"opqrs",
+					"efghi",
+					"ihgef",
+					"abdde",
+					"srqpo",
+					"abdde",
+					"fghij",
+					"efghi",
+					"jklmn",
+					"opqrs",
+					"tuvwx",
+					"abcde",
+				},
+			},
+			want: []string{
+				"abcde",
+				"efghi",
+				"jklmn",
+				"opqrs",
+				"tuvwx",
+			},
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := WordList(tc.args.collisonMap, tc.args.fullWordList)
 			assert.Equal(t, tc.want, got)
 		})
 	}
